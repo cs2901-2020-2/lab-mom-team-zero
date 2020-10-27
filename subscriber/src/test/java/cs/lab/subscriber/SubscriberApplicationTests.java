c1148fbec33a85c4ff47e798c895407b5383b13b
root <root@DESKTOP-OO1V4NK.localdomain>
2020-10-27 10:33:42 -0500

    ps

---
+++ subscriber/src/test/java/cs/lab/subscriber/SubscriberApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.lab.subscriber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
