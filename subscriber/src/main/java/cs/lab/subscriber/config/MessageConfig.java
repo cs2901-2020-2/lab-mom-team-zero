5882c2104141995bf3fc257338ee8c8dac272ad7
root <root@DESKTOP-OO1V4NK.localdomain>
2020-10-28 10:46:09 -0500

    receiver

--- subscriber/src/main/java/cs/lab/subscriber/config/MessageConfig.java
+++ subscriber/src/main/java/cs/lab/subscriber/config/MessageConfig.java
@@ -1,18 +1,13 @@
 package cs.lab.subscriber.config;
+import cs.lab.subscriber.controller.SubscriberController;
 import org.springframework.amqp.core.*;
-import cs.lab.subscriber.Receiver;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-//import javax.sound.midi.Receiver;
 
 
 @Configuration
@@ -20,7 +15,6 @@ public class MessageConfig {
 
     public static final String QUEUE = "queue";
     public static final String EXCHANGE = "fanout";
-//public static final String ROUTINGKEY = "routingKey";
 
     public MessageConfig sender() {
         return new MessageConfig();
@@ -32,22 +26,9 @@ public class MessageConfig {
     }
 
     @Bean
-    public Queue queue(){
-        //return new Queue(QUEUE);
+    public Queue queue1(){
         return new AnonymousQueue();
     }
-
-
-    public Queue queue2(){
-        //return new Queue(QUEUE);
-        return new AnonymousQueue();
-    }
-/*
-    @Bean
-    public TopicExchange exchange(){
-        return new TopicExchange(EXCHANGE);
-    }
-*/
 
     @Bean
     public Binding binding(FanoutExchange fanout, Queue queue1){
@@ -55,12 +36,7 @@ public class MessageConfig {
     }
 
     @Bean
-    public Binding binding2(FanoutExchange fanout, Queue queue2){
-        return BindingBuilder.bind(queue2).to(fanout);
-    }
-
-    @Bean
-    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+    MessageListenerAdapter listenerAdapter(SubscriberController receiver) {
         return new MessageListenerAdapter(receiver, "receiveMessage");
     }
     @Bean
--- subscriber/src/main/java/cs/lab/subscriber/controller/SubscriberController.java
+++ subscriber/src/main/java/cs/lab/subscriber/controller/SubscriberController.java
@@ -1,53 +1,17 @@
 package cs.lab.subscriber.controller;
-
-
-import cs.lab.subscriber.config.MessageConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.AmqpException;
-import org.springframework.amqp.core.FanoutExchange;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.connection.Connection;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionListener;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jca.cci.CciOperationNotSupportedException;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.swing.*;
-import java.nio.channels.Channel;
-
-@RestController
-@RequestMapping(path = "/consum")
-
 
 @Component
 public class SubscriberController {
 
-    String in;
     private static final Logger log = LoggerFactory.getLogger(SubscriberController.class);
 
-    @RabbitListener(queues = MessageConfig.QUEUE)
-    public String consumer(String in){
-        this.in = in;
-
-        log.info("message received " +   in);
-        return "success!! " + in;
+    public void receiveMessage(String message) {
+        log.info("Received <" + message + ">");
     }
-
-    @GetMapping(path = "/consum")
-    public @ResponseBody String showData(){
-        return in;
-    }
-
-
-
-
 }
+
 
 
